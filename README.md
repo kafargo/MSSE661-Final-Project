ae22df1a7a72fe9862671bf8c6145e76086d2372
alexfargo <kevin.a.fargo@gmail.com>
2024-03-26 09:40:41 -0600

    Added a web server

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MSSE661-Final-Project
+
+This is a node based project using and express server to serve up content.
+
---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use('/js', express.static(__dirname + '/public/src'));
+app.use('/html', express.static(__dirname + '/public/html'));
+app.use('/images', express.static(__dirname + '/public/images'));
+
+
+app.listen(3000, function(){
+    console.log('Server started running at http://localhost:%s', 3000);
+});
